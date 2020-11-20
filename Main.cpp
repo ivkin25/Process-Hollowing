77d6e06cd3131cef860f80a11ecbdcef1efde6c5
Daniel Ivkin <danielivkin121@gmail.com>
2020-11-20 19:27:43 +0200

    Remove the unnecessary class ProcessHollowing

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,22 @@
-#include "ProcessHollowing.hpp"
+#include "HollowingFunctions.hpp"
+#include "Hollowing64Bit.hpp"
+#include "Hollowing32Bit.hpp"
+#include <string>
+
+template<typename T>
+bool tryConstructProcessHollowing(HollowingFunctions** value, const std::string target, const std::string& payload)
+{
+    try
+    {
+        *value = new T(target, payload);
+
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+}
 
 int main(int argc, char* argv[])
 {
